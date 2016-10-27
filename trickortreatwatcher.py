f51172657c37302d85fd59be0cc33e2c4177fa41
PiDiaries <iainjohnston79@gmail.com>
2016-10-27 17:47:53 +0100

    Update trickortreatwatcher.py

--- trickortreatwatcher.py
+++ trickortreatwatcher.py
@@ -1,5 +1,6 @@
 from gpiozero import  LED, MotionSensor, PWMLED, LightSensor
 import time
+from datetime import datetime
 import os
 import glob
 import sys
@@ -8,6 +9,18 @@ CONSUMER_KEY = ' ***Your info *** '
 CONSUMER_SECRET = ' ***Your info *** '
 ACCESS_KEY = ' ***Your info *** '
 ACCESS_SECRET = ' ***Your info *** '
+
+from gpiozero import  LED, MotionSensor, PWMLED, LightSensor
+import time
+from datetime import datetime
+import os
+import glob
+import sys
+from twython import Twython
+CONSUMER_KEY = ' ***Your Info *** '
+CONSUMER_SECRET = '***Your Info ***'
+ACCESS_KEY = '***Your Info ***'
+ACCESS_SECRET = '***Your Info ***'
 
 pir = MotionSensor(18)
 red1 = LED(20)
@@ -24,39 +37,25 @@ device_file = device_folder + '/w1_slave'
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
 
-def read_temp_raw():
-    f = open(device_file, 'r') # Opens the temperature device file
-    lines = f.readlines() # Returns the text
-    f.close()
-    return lines
+tempfile = open("/sys/bus/w1/devices/28-041591c231ff/w1_slave")
+thetext = tempfile.read()
+tempfile.close()
+tempdata = thetext.split("\n")[1].split(" ")[9]
+temperature = float(tempdata[2:])
+temperature = str(temperature / 1000)
 
-def read_temp():
-    lines = read_temp_raw()
-
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(60)
-        lines = read_temp_raw()
-
-    equals_pos = lines[1].find('t=')
-
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
-
-
+thetime = datetime.now().strftime('%-I:%M%P on %d-%m-%Y')
 
 while True:
         ldr.wait_for_dark()
         print("Ready")
         pir.wait_for_motion()
         print("Motion Detected")
-        print(read_temp())
         print(ldr.value)
         red1.on()
         red2.on()
         green.pulse(fade_in_time=1.5, fade_out_time=1.5, n=4, background=True)
-        api.update_status(status='Tweet test '+temp+' c')
+        api.update_status(status= temperature + " C at " + thetime + " #Raspberrypitrickortreat")
         time.sleep(10)
         red1.off()
         red2.off()
